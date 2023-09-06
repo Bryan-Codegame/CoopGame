e57648e77ebbe085c03f6fab676ac7711d009630
Bryan Escobar <bryanesc26@gmail.com>
2023-09-06 01:17:12 -0600

    LineTracing - spawn from GetActorEyesViewPoint

--- Source/CoopGame/Private/SWeapon.cpp
+++ Source/CoopGame/Private/SWeapon.cpp
@@ -3,6 +3,8 @@
 
 #include "SWeapon.h"
 
+#include "Editor/EditorEngine.h"
+
 // Sets default values
 ASWeapon::ASWeapon()
 {
@@ -19,6 +21,35 @@ void ASWeapon::BeginPlay()
 {
 	Super::BeginPlay();
 	
+}
+
+void ASWeapon::Fire()
+{
+	AActor* MyOwner = GetOwner();
+
+	if(MyOwner)
+	{
+		FVector EyeLocation;
+		FRotator EyeRotation;
+		MyOwner->GetActorEyesViewPoint(EyeLocation, EyeRotation);
+
+		FVector TraceEnd = EyeLocation + (EyeRotation.Vector() * 10000);
+		
+		FCollisionQueryParams QueryParams;
+		QueryParams.AddIgnoredActor(MyOwner);
+		QueryParams.AddIgnoredActor(this);
+		/*it will trace against each individual triangle of the mesh that we're hitting.
+		 * this is more expensive, but it also gives us the exact result of where we hit something. */
+		QueryParams.bTraceComplex = true;
+
+		FHitResult Hit;
+		if(GetWorld()->LineTraceSingleByChannel(Hit, EyeLocation, TraceEnd, ECC_Visibility, QueryParams))
+		{
+			//Blocking hit! Process damage
+		}
+
+		DrawDebugLine(GetWorld(), EyeLocation, TraceEnd, FColor::Red, false,1.0f, 0, 1.0f);
+	}
 }
 
 // Called every frame
