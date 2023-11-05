415b4bfbc91931eb6d5a9c2fcd27f4fe076a1ef7
Bryan Escobar <bryanesc26@gmail.com>
2023-11-05 00:59:50 -0600

    Created a HealthComponent to magane the health of all actors

---
+++ Source/CoopGame/Private/SHealthComponent.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SHealthComponent.h"
+
+// Sets default values for this component's properties
+USHealthComponent::USHealthComponent()
+{
+	Health = 200.f;
+}
+
+
+// Called when the game starts
+void USHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+
---
+++ Source/CoopGame/Public/SHealthComponent.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "SHealthComponent.generated.h"
+
+
+UCLASS( ClassGroup=(COOP), meta=(BlueprintSpawnableComponent) )
+class COOPGAME_API USHealthComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	USHealthComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Health Component")
+	float Health;
+	
+};
